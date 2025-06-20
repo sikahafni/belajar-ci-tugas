8c82456d0c73c0f909337f498dead5132fcd1994
sikahafni <afifahaf0937@gmail.com>
2025-06-20 11:30:49 +0700

    menambahkan api data transaksi pembelian

--- app/Views/components/sidebar.php
+++ app/Views/components/sidebar.php
@@ -25,6 +25,12 @@
                     <span>Produk</span>
                 </a>
             </li><!-- End Produk Nav -->
+            <li class="nav-item">
+    <a class="nav-link <?php echo (uri_string() == 'profile') ? "" : "collapsed" ?>" href="profile">
+        <i class="bi bi-person"></i>
+        <span>Profile</span>
+    </a>
+</li><!-- End Profile Nav -->
         <?php
         }
         ?>
